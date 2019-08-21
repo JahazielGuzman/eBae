115234a3b40aba4d58c62c86cc0e2a602757edf3
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-08-21 09:37:00 -0400

    Slap search results on DOM

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -3,6 +3,11 @@ class ItemsController < ApplicationController
 	def index
 		search_query = "%" + params[:search].split(' ').join('%') + "%"
 		items = Item.where("name LIKE ?", search_query)
+		items = items.map{|item| {id: item.id, name: item.name, description: item.description, 
+															price: item.price,state: item.state, img_url: item.img_url,
+															user: item.user
+															}
+														}
 		render json: items
 	end
 
