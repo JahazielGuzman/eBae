d60ca934d40e0f5dfc46aa9bc9d09862044a4ffd
github username <github email address>
2019-08-21 10:29:50 -0400

    divs worked out

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,35 +3,25 @@ document.addEventListener("DOMContentLoaded", function(){
 		.then(res => res.json())
 		.then(itemIndexer)
 
+
 })
+
+const overlayDiv = document.getElementById('overlay')
+const specialBoxDiv = document.getElementById('specialBox')
+const body = document.getElementById('body')
+const wrapper = document.getElementById('wrapper')
+overlayDiv.className = 'overlay'
+specialBoxDiv.className = 'specialBox'
 
 function itemIndexer(items){
 	console.log(items)
 	items.forEach(listItems)
 }
 
-function listItems(item){
-	const body = document.getElementById('body')
-	const wrapper = document.getElementById('wrapper')
-	
 
-	const indexDiv = document.createElement('div')
-		indexDiv.innerHTML = `<h1>${item.name}</h1>`
-		wrapper.appendChild(indexDiv)
+wrapper.addEventListener("click", (e) => {
 
-
-	const overlayDiv = document.createElement('div')
-		overlayDiv.className = 'overlay'
-		body.appendChild(overlayDiv)
-
-
-	const specialBoxDiv = document.createElement('div')
-		specialBoxDiv.className = 'specialBox'
-		specialBoxDiv.innerHTML = `<h1>${item.description}</h1>`
-		body.appendChild(specialBoxDiv)	
-		
-
-	indexDiv.addEventListener("click", () => {
+	if (e.target.parentElement.className === "item-card") {	
 		overlayDiv.style.opacity = .8;
 		// specialBoxDiv.appendChild(overlayBtn)
 
@@ -42,21 +32,42 @@ function listItems(item){
 			overlayDiv.style.display = "block";
 			specialBoxDiv.style.display = "block";
 		}
-	})
 
-	overlayDiv.addEventListener("click", () => {
-		overlayDiv.style.opacity = 0;
-		
-		if(overlayDiv.style.display == "block"){
-			overlayDiv.style.display = "none";
-			specialBoxDiv.style.display = "none";
-		} else {
-			overlayDiv.style.display = "block";
-			specialBoxDiv.style.display = "block";
-		}
-	})
+		specialBoxDiv.innerHTML = `<h1>name${e.target.parentElement.dataset.name}</h1>
+					<h1>desc${e.target.parentElement.dataset.description}</h1>
+					<h1>price${e.target.parentElement.dataset.price}</h1>
+					<h1>state${e.target.parentElement.dataset.state}</h1>
+					<h1>img${e.target.parentElement.dataset.img_url}</h1>
+					<h1>user${e.target.parentElement.dataset.id}</h1>`
+	}
+})
+overlayDiv.addEventListener("click", () => {
+	overlayDiv.style.opacity = 0;
+	
+	if(overlayDiv.style.display == "block"){
+		overlayDiv.style.display = "none";
+		specialBoxDiv.style.display = "none";
+	} else {
+		overlayDiv.style.display = "block";
+		specialBoxDiv.style.display = "block";
+	}
+})
+
+function listItems(item){
+
+	const indexDiv = document.createElement('div')
+		indexDiv.className = "item-card"
+		indexDiv.innerHTML = `<h1>${item.name}</h1>`
+		indexDiv.dataset.description = item.description;
+		indexDiv.dataset.name = item.name
+		indexDiv.dataset.price = item.price
+		indexDiv.dataset.state = item.state
+		indexDiv.dataset.img_url = item.img_url
+		indexDiv.dataset.id = item.user_id
+
+		wrapper.appendChild(indexDiv)
+
 }
-
 
 
 // function searchFunction() {
