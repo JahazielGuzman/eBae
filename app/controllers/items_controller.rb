0a65d504ad94c5ba25d6ee383ade677fe69e3361
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-10-28 15:24:30 -0400

    Now search works better, case does not matter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
 	def index
 		if params[:search]
 			search_query = "%" + params[:search].split(' ').join('%') + "%"
-			items = Item.where("name LIKE ?", search_query)
+			items = Item.where("LOWER(name) LIKE LOWER(?)", search_query)
 			items = items.map{|item| {id: item.id, name: item.name, description: item.description, 
 					price: item.price,state: item.state, img_url: item.img_url,
 					user: item.user
