9eb57e94fa40cf8c7617037be71afb1be971d91b
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-10-29 20:09:26 -0400

    add routes for bought and sold items for a logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/login", to: "users#login"
   get "/bought_items/:id", to: "users#bought_items"
+  get "/selling_items/:id", to: "users#selling_items"
   resources :items
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
