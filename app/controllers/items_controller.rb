e8901ecfa41998a777647f04ec87d8968ae0a56f
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-12-08 21:22:12 -0500

    now if user is logged in the API will not return items that belong to that user in the initialItems

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,32 @@
 class ItemsController < ApplicationController
 
 	def index
-		# check for params[:user_id], if it is populated then change query to 
-		# return items that are not being sold by the user
 		if params[:search]
 			search_query = "%" + params[:search].split(' ').join('%') + "%"
 			# only include items that are being sold
-			items = Item.where("LOWER(name) LIKE LOWER(?)", search_query)
-			items = items.map{|item| {id: item.id, name: item.name, description: item.description, 
-					price: item.price,state: item.state, img_url: item.img_url,
-					user_id: item.user, user_name: item.user.name
-					}
-			}
+			items = Item.where("LOWER(name) LIKE LOWER(?) AND user_id != ?", search_query)
 		else
 			items = Item.where(state: "sell")
-			items = items.map{|item| {id: item.id, name: item.name, description: item.description, 
-				price: item.price,state: item.state, img_url: item.img_url,
-				user_id: item.user.id, user_name: item.user.name
-				}
-			}
 		end
+
+		# check for params[:user_id], if it is populated then change query to 
+		# return items that are not being sold by the user
+		if params[:user_id]
+			items = items.where.not(user_id: params[:user_id])
+		end
+
+		items = items.map{|item| 
+				{
+					id: item.id, 
+					name: item.name, 
+					description: item.description, 
+					price: item.price,state: item.state, 
+					img_url: item.img_url,
+					user_id: item.user.id, 
+					user_name: item.user.name
+				}
+		}
+
 		render json: items
 	end
 
